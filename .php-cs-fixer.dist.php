95b047432a711added43d718423391892dfbca2e
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-05-09 01:23:47 +0300

    CS fixer updated

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
-return PhpCsFixer\Config::create()
-    ->setCacheFile(__DIR__.'/.php_cs.cache')
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRules([
-        '@Symfony' => true
+        '@Symfony' => true,
     ])
     ->setFinder($finder)
 ;
